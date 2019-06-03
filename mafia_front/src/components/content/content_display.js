3d55db93110a773db2419a90aac87a6540f94dfd
Eugeniy <eugeniyshmarev@yandex.tu>
2019-06-03 17:21:42 +0300

    added front flex boxes

---
+++ mafia_front/src/components/content/content_display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './display.css';
+
+class Display extends React.Component{
+    render() {
+        return (
+            <div id='display'>
+
+            </div>
+        );
+    }
+}
+
+export default Display;
