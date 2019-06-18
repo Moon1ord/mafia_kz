a0499c3f216d22585121e8ea3a926c414926414b
Евгений Шмарев <evgenijsmarev@keenetic_ultra.ovapress.ru>
2019-06-18 15:44:47 +0300

    game controller routing

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
 namespace DefaultNamespace
 {
-    public class GameController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<IActionResult> GetGame()
+        {
+            return Ok();
+        }
     }
 }
