ed0631679bb234dd9faccd5b89af6e421337d2c9
Moon1ord <eugeniyshmarev@yandex.ru>
2019-06-18 18:11:43 +0300

    Added db context to controller

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 using System.Threading.Tasks;
+using mafia_kz.Models;
+
 namespace DefaultNamespace
 {
     [Route("api/[controller]")]
     [ApiController]
     public class GameController : ControllerBase
     {
+        private readonly MafiaDbContext _context;
+
+        public GameController(MafiaDbContext context)
+        {
+            _context = context;
+        }
+        
         [HttpGet]
         public async Task<IActionResult> GetGame()
         {
