86edffc7f0199c6ea6313912d7549b441f2582ff
Moon1ord <eugeniyshmarev@yandex.ru>
2019-08-02 15:08:19 +0300

    developing game routing

---
+++ mafia_front/src/components/Mafia_kz/Mafia.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Mafia extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Hello</p>
+            </div>
+        );
+    }
+
+}
+
+export default Mafia;
